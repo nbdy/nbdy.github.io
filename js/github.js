6c838f89425cc82674c75c154148a12d36b6765d
smthnspcl <pascal@eberlein.io>
2019-12-22 02:20:07 +0100

    update

--- js/github.js
+++ js/github.js
@@ -1,4 +1,17 @@
-
+let Request = {
+    request: function (url, method, data, cb){
+        let r = new XMLHttpRequest();
+        r.addEventListener("load", cb);
+        r.open(method, "https://crossorigin.me/" + url);
+        r.send(data);
+    },
+    get: function (url, cb) {
+        Request.request(url, "GET", null, cb);
+    },
+    post: function (url, data, cb) {
+        Request.request(url, "POST", null, cb);
+    }
+};
 
 function Github(username) {
     let github = {
@@ -7,23 +20,18 @@ function Github(username) {
         repositories: [],
         getRepositories: null,
         allFunctions: [],
+
+        _cbGetRepositories: function (data) {
+            console.log(data);
+        },
     };
 
     github.getRepositories = function getRepositories() {
-        let url = github.url + "stab=repositories";
-        console.log(url);
-        $.get(url, function (data) {
-            let html = $("<div></div>");
-            html.html = data;
-            console.log(data);
-            let repos = $("ul[value='your-repos-filter']");
-            console.log(repos);
-        });
+        Request.get(github.url, github._cbGetRepositories);
     };
 
     github.allFunctions = [
         github.getRepositories,
-
     ];
 
     return github;
